77e7686a2c1f63895da0057f34e5a8bc96755446
unknown <wonjune0329@gmail.com>
2025-11-07 10:19:24 +0900

    수정

--- portfolio-new/src/index.js
+++ portfolio-new/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { HashRouter } from 'react-router-dom'; // ← 추가
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>         {/* ← 여기로 감싸기 */}
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
